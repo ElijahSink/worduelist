617f3f8d1ad751344d33426b2bd5b968e26dd1a0
Elijah Sink <theminorprofit@gmail.com>
2022-02-03 17:54:48 -0500

    Change port envvar again

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ def main() -> None:
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
     TOKEN = os.environ["BOT_TOKEN"]
-    PORT = int(os.environ["WEBHOOK_PORT"])
+    PORT = int(os.environ["PORT"])
     WEBHOOK_URL = os.environ["WEBHOOK_URL"]
 
     updater = Updater(TOKEN)
